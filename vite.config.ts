058307073da1f1697bf7fcf6b97b29e292c10905
Diyaa <36938682+Diyaa-Gubarah@users.noreply.github.com>
2024-07-31 19:35:01 +0300

    update assest path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  assetsInclude: ["./public/assets/*"],
+  assetsInclude: ["./public/images/*"],
   base: '/tapshelf/', 
   plugins: [react(), macrosPlugin()],
   build: {
