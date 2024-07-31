0ebb109ddcbc01099a69ab7235c8b7afc3a828aa
Diyaa <36938682+Diyaa-Gubarah@users.noreply.github.com>
2024-07-31 19:33:34 +0300

    update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import macrosPlugin from "vite-plugin-babel-macros";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tapshelf/', // Replace 'your-repo-name' with your actual repository name
-  plugins: [react()],
+  assetsInclude: ["./public/assets/*"],
+  base: '/tapshelf/', 
+  plugins: [react(), macrosPlugin()],
+  build: {
+    outDir: "dist", // Output directory
+    sourcemap: true, // Enable source maps
+  },
 });
